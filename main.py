83cfb60f097de56e4f919bb5590fafd056e93337
Faraz Abdul Nasir <63238584+Faraz-Nasir@users.noreply.github.com>
2020-12-02 04:37:07 +0530

    Delete main.py

--- main.py
+++
@@ -1,7 +0,0 @@
-import requests
-from bs4 import BeautifulSoup as bs
-
-res=requests.get("https://en.wikipedia.org/wiki/List_of_Walt_Disney_Pictures_films")
-b=bs(res.text,'html.parser')
-
-print(b)
